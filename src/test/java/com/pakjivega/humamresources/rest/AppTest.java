ca65d12679bc22b24c2714c0aac5152458ac1345
pakjivega <pakjivega@gmail.com>
2019-06-21 00:11:17 +0200

    First commit

---
+++ src/test/java/com/pakjivega/humamresources/rest/AppTest.java
@@ -0,0 +1,19 @@
+package com.pakjivega.humamresources.rest;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        Assert.assertTrue(true);
+    }
+}
